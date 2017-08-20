8d3940326966c368758ade219296d3249245c8ca
alfredlucero <alfred.lucero@ucla.edu>
2017-08-20 12:13:49 -0700

    Finished redux notes section

--- redux_notes.jsx
+++ redux_notes.jsx
@@ -256,9 +256,422 @@ var removeHobby = (id) => {
 // To call actions: store.dispatch(addHobby('running'));
 // store.dispatch(removeHobby(1));
 
+// Asynchronous reducers and actions
+var mapReducer = (state = { isFetching: false, url: undefined }, action) => {
+  switch (action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCH';
+      return {
+        isFetching: false,
+        url: action.url
+      };
+    default:
+      return state;
+  }
+};
+
+var startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCH'
+  };
+};
+
+var completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  };
+};
+
+var fetchLocation = () => {
+  // Might render a loading wheel here
+  store.dispatch(startLocationFetch());
+
+  // Make asynchronous request to third party API
+  // using axios
+  axios.get('http://ipinfo.io').then(function(res) {
+    var loc = res.data.loc;
+    var baseUrl = 'http://maps.google.com?q=';
+
+    // Finish the loading and render data related to location
+    store.dispatch(completeLocationFetch(baseUrl + loc));
+  });
+};
+
 var reducer = redux.combineReducers({
   // name state managed by nameReducer
   name: nameReducer,
   // hobbies array state managed by hobbiesReducer
-  hobbies: hobbiesReducer
+  hobbies: hobbiesReducer,
+  map: mapReducer
 });
+
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+
+  if (state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading...'; 
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">View your Location</a>';
+  }
+});
+
+// Sample directory structure
+// actions folder, components folder, reducers folder, store folder
+// index.jsx inside actions, reducers; configureStore.jsx inside store
+
+// Actions to test
+export var setSearchText = (searchText) => {
+  return {
+    type: 'SET_SEARCH_TEXT',
+    searchText
+  };
+};
+
+export var addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    text
+  };
+};
+
+// Creating and Testing Actions
+var expect = require('expect');
+var actions = require('actions');
+
+describe('Actions', () => {
+  it('should generate search text action', () => {
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'Some search text'
+    };
+    
+    var result = actions.setSearchText(action.searchText);
+    expect(result).toEqual(action);
+  });
+
+  it('should generate add todo action', () => {
+    var action = {
+      type: 'ADD_TODO',
+      text: 'Todo text'
+    };
+
+    var result = actions.addTodo(action.text);
+    expect(result).toEqual(action);
+  });
+});
+
+// Creating and testing reducers
+// Can use deep-freeze-strict to make sure arguments aren't changed
+// or updated like setting action.something = 2 -> would fail when calling df(argument)
+export var searchTextReducer = (state = '', action) => {
+  switch (action.type) {
+    case 'SET_SEARCH_TEXT':
+      return action.searchText;
+    default:
+      return state;
+  }
+};
+
+var expect = require('expect');
+var reducers = require('reducers');
+
+describe('Reducers', () => {
+  describe('searchTextReducer', () => {
+    it('should set searchText', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'dog'
+      };
+
+      var res = reducers.searchTextReducer('', action);
+      expect(res).toEqual(action.searchText);
+    });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+  });
+});
+
+// Wiring up redux in a TodoApp 
+var React = require('react');
+var ReactDOM = require('react-dom');
+var { Provider } = require('react-redux');
+var { Route, Router, IndexRoute, hashHistory } = require('react-router');
+
+var TodoApp = require('TodoApp');
+
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+  // Taking advantage of localStorage here
+  TodoApi.setTodos(state.todos);
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
+
+$(document).foundation();
+
+require('style!css!sass!applicationStyles');
+
+// Provider lets all of our components under
+// TodoApp access the store and dispatch actions
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp/>
+  </Provider>,
+  document.getElementById('app');
+);
+
+// Sample TodoList.jsx, no need to pass data down from TodoApp anymore
+var React = require('react');
+var { connect } = require('react-redux');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+  render: function() {
+    var { todos } = this.props;
+    var renderTodos = () => {
+      if (todos.length === 0) {
+        return (
+          <p className="container__message">Nothing To Do</p>
+        );
+      }
+
+      // We also don't need to pass the toggleHandler anymore down to the Todo component
+      // because it will only dispatch an action now
+      return todos.map((todo) => {
+        return (
+          <Todo key={todo.id} {...todo}/>
+        );
+      })
+    };
+
+    return (
+      <div>
+        {renderTodos()}
+      </div>
+    );
+  }
+});
+
+// Connecting TodoList to our Provider to request
+// data it needs to render; first argument in connect
+// corresponds to the state data it wants back and passed through props
+module.exports = connect(
+  (state) => {
+    return {
+      todos
+    };
+  }
+)(TodoList);
+
+// Sample Todo.jsx
+var React = require('react');
+var { connect } = require('react-redux');
+var moment = require('moment');
+var actions = require('actions');
+
+export var Todo = React.createClass({
+  render: function() {
+    var { id, text, completed, createdAt, completedAt, dispatch } = this.props;
+    var todoClassName = completed ? 'todo todo-completed' : 'todo';
+    var renderDate = () => {
+      var message = 'Created ';
+      var timestamp = createdAt;
+
+      if (completed) {
+        message = 'Completed ';
+        timestamp = completedAt;
+      }
+
+      return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+    };
+
+    return (
+      <div className={todoClassName} onClick=(() => {
+        dispatch(actions.toggleTodo(id));
+      })>
+        <div>
+          <input type="checkbox" checked={completed}/>
+        </div>
+        <div>
+          <p>{text}</p>
+          <p className="todo__subtext">{renderDate()}</p>
+        </div>
+      </div>
+    );
+  }
+});
+
+// Doesn't need more data since already passed through props from TodoList
+// but we can also get access to dispatch method from props
+export default connect()(Todo);
+
+// Sample Todo.test.jsx
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jquery');
+
+var { Todo } = require('Todo');
+
+describe('Todo', () => {
+  it('should exist', () => {
+    expect(Todo).toExist();
+  });
+
+  it('should dispatch TOGGLE_TODO action on click', () => {
+    var todoData = {
+      id: 199,
+      text: 'Write todo.test.jsx test',
+      completed: true
+    };
+
+    var spy = expect.createSpy();
+    var todo = TestUtils.renderIntoDocument(<Todo {...todoData} dispatch={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(todo));
+
+    TestUtils.Simulate.click($el[0]);
+
+    expect(spy).toHaveBeenCalledWith({
+      type: 'TOGGLE_TODO',
+      id: todoData.id
+    });
+  });
+});
+
+// Sample TodoApp.test.jsx
+var React = require('react');
+var ReactDOM = require('react-dom');
+var { Provider } = require('react-redux');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jquery');
+
+var configureStore = require('configureStore');
+var TodoApp = require('TodoApp');
+var TodoList = require('TodoList');
+
+describe('TodoApp', () => {
+  it('should exist', () => {
+    expect(TodoApp).toExist();
+  });
+
+  it('should render TodoList', () => {
+    var store = configureStore.configure();
+    var provider = TestUtils.renderIntoDocument(
+      <Provider store={store}>
+        <TodoApp/>
+      </Provider>
+    );
+
+    var todoApp = TestUtils.scryRenderedComponentsWithType(provider, TodoApp)[0];
+    var todoList = TestUtils.scryRenderedComponentsWithType(todoApp, TodoList);
+
+    expect(todoList.length).toEqual(1);
+  });
+});
+
+// Refactored TodoSearch.jsx
+var React = require('react');
+var { connect } = require('react-redux');
+var actions = require('actions');
+
+export var TodoSearch = React.createClass({
+  render: function() {
+    var { dispatch, showCompleted, searchText } = this.props;
+
+    return (
+      <div className="container__header">
+        <div>
+          <input type="search" ref="searchText" placeholder="Search todos" value={searchText} onChange={() => {
+            var searchText = this.refs.searchText.value;
+            dispatch(actions.setSearchText(searchText))
+          }}/>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" ref="showCompleted" checked={showCompleted} onChange={() => {
+              dispatch(actions.toggleShowCompleted());
+            }}/>
+            Show Completed Todos
+          </label>
+        </div>
+      </div>
+    );
+  }
+});
+
+export default connect(
+  (state) => {
+    return {
+      showCompleted,
+      searchText
+    };
+  }
+)(TodoSearch);
+
+// Sample TodoSearch.test.jsx
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jquery');
+
+import { TodoSearch } from 'TodoSearch';
+
+describe('TodoSearch', () => {
+  it('should exist', () => {
+    expect(TodoSearch).toExist();
+  });
+
+  it('should dispatch SET_SEARCH_TEXT on input change', () => {
+    var searchText = 'Hello';
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText
+    };
+    var spy = expect.createSpy();
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch dispatch={spy}/>);
+
+    todoSearch.refs.searchText.value = searchText;
+    TestUtils.Simulate.change(todoSearch.refs.searchText);
+
+    expect(spy).toHaveBeenCalledWith(action);
+  });
+
+  it('should call onSearch with proper checked value', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+    var spy = expect.createSpy();
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch dispatch={spy}/>);
+
+    todoSearch.refs.showCompleted.checked = true;
+    TestUtils.Simulate.change(todoSearch.refs.showCompleted):
+
+    expect(spy).toHaveBeenCalledWith(action)
+  });
+});
+
+
